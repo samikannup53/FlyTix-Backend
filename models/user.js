6d19e5e71d7a6840fd997a98a902590e3422d480
samikannup53 <samikannup53@gmail.com>
2025-06-21 22:56:36 +0530

    Create User Model with Schema

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    dob: { type: Date, required: true },
+    gender: { type: String, enum: ["Male", "Female", "Other"], required: true },
+    password: { type: String, required: true },
+    termsAccepted: { type: Boolean, required: true },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
