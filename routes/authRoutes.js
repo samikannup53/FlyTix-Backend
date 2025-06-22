f5887e350621207ad5ac98e7122ce61d47106a6c
samikannup53 <samikannup53@gmail.com>
2025-06-22 09:54:00 +0530

    Add Route for User Login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const registerUser = require("../controllers/authController/registerUser");
+const loginUser = require("../controllers/authController/loginUser");
 
 // Routes
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
