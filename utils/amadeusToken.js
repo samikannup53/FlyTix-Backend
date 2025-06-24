b623fcbc0cba304b5949b1031f17c59e9db320dd
samikannup53 <samikannup53@gmail.com>
2025-06-24 19:37:20 +0530

    Add Get Amadeus Token Function

---
+++ utils/amadeusToken.js
@@ -0,0 +1,36 @@
+const fetch = require("node-fetch");
+
+let tokenCache = {
+  token: null,
+  expiry: null,
+};
+
+async function getAmadeusToken() {
+  const now = Date.now();
+
+  if (tokenCache.token && tokenCache.expiry > now) {
+    return tokenCache.token;
+  }
+
+  const res = await fetch(
+    "https://test.api.amadeus.com/v1/security/oauth2/token",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({
+        grant_type: "client_credentials",
+        client_id: process.env.AMADEUS_CLIENT_ID,
+        client_secret: process.env.AMADEUS_CLIENT_SECRET,
+      }),
+    }
+  );
+
+  const data = await res.json();
+
+  tokenCache.token = data.access_token;
+  tokenCache.expiry = now + data.expires_in * 1000;
+
+  return data.access_token;
+}
+
+module.exports = getAmadeusToken;
