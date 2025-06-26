958c223368e1718d84d837bc4adbed5794df2571
samikannup53 <samikannup53@gmail.com>
2025-06-26 12:53:51 +0530

    Add Airport Data JSON file and Optimized Airport Details

--- utils/summarizeAmadeusFlight.js
+++ utils/summarizeAmadeusFlight.js
@@ -1,4 +1,19 @@
+const airportData = require("../data/airports.json");
+
+function getAirportDetails(iataCode) {
+  const airport = airportData[iataCode];
+  return {
+    airportName: airport?.name || "N/A",
+    city: airport?.city || "N/A",
+    state: airport?.state || "N/A",
+    country: airport?.country || "N/A",
+  };
+}
+
 function formatSegment(segment) {
+  const departureDetails = getAirportDetails(segment.departure.iataCode);
+  const arrivalDetails = getAirportDetails(segment.arrival.iataCode);
+
   return {
     airlineCode: segment.carrierCode,
     flightNumber: `${segment.carrierCode}-${segment.number}`,
@@ -7,7 +22,10 @@ function formatSegment(segment) {
       date: segment.departure?.at.split("T")[0],
       time: segment.departure?.at.split("T")[1],
       cityCode: segment.departure.iataCode,
-      airport: segment.departure.airportName || "N/A",
+      city: departureDetails.city,
+      state: departureDetails.state,
+      country: departureDetails.country,
+      airport: departureDetails.airportName,
       terminal: segment.departure.terminal || "N/A",
     },
 
@@ -15,7 +33,10 @@ function formatSegment(segment) {
       date: segment.arrival?.at.split("T")[0],
       time: segment.arrival?.at.split("T")[1],
       cityCode: segment.arrival.iataCode,
-      airport: segment.arrival.airportName || "N/A",
+      city: arrivalDetails.city,
+      state: arrivalDetails.state,
+      country: arrivalDetails.country,
+      airport: arrivalDetails.airportName,
       terminal: segment.arrival.terminal || "N/A",
     },
   };
@@ -37,14 +58,18 @@ function summarizeAmadeusFlight(flight, meta = {}) {
     validatingAirline: flight.validatingAirlineCodes?.[0] || "N/A",
 
     outbound: {
-      segments: outbound.segments.map(formatSegment),
+      segments: outbound.segments.map((segment) => {
+        return formatSegment(segment);
+      }),
       duration: outbound.duration,
       stops: outbound.segments.length - 1,
     },
 
     returnTrip: returnTrip
       ? {
-          segments: returnTrip.segments.map(formatSegment),
+          segments: outbound.segments.map((segment) => {
+            return formatSegment(segment);
+          }),
           duration: returnTrip.duration,
           stops: returnTrip.segments.length - 1,
         }
