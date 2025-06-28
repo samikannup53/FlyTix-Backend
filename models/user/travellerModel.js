e0923cba6de5a90d760b7cbdff3e3d47c1be8c3e
samikannup53 <samikannup53@gmail.com>
2025-06-28 17:12:28 +0530

    Add Traveller Schema

---
+++ models/user/travellerModel.js
@@ -0,0 +1,19 @@
+const mongoose = reuire("mongoose");
+
+const travellerSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  age: { type: Number, required: true },
+  gender: { type: String, enum: ["Male", "Female", "Other"], required: true },
+  catagory: {
+    type: String,
+    enum: ["Adult", "Child", "Infant"],
+    required: true,
+  },
+});
+
+const Traveller = mongoose.model("Traveller", travellerSchema);
+
+module.exports = Traveller;
