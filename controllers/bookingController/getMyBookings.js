1247d0926057ea7e610842f79c94788a21a5f07b
samikannup53 <samikannup53@gmail.com>
2025-06-28 15:34:07 +0530

    Add Get My Bookings with Router and Boiler Plate Code

---
+++ controllers/bookingController/getMyBookings.js
@@ -0,0 +1,5 @@
+async function getMyBookings(req, res) {
+  res.json({ msg: "Get My Bookings Route is Working...." });
+}
+
+module.exports = getMyBookings;
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const initiateBooking = require("../controllers/bookingController/initiateBooking");
 const cancelBooking = require("../controllers/bookingController/cancelBooking");
 const rescheduleBooking = require("../controllers/bookingController/rescheduleBooking");
+const getMyBookings = require("../controllers/bookingController/getMyBookings");
 
 router.post("/initiate", initiateBooking);
 router.post("/cancel", cancelBooking);
 router.post("/reschedule", rescheduleBooking);
+router.get("/mybookings", getMyBookings);
 
 module.exports = router;
