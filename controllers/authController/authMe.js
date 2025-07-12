60c186bd8a411c08b69d9bb0d49d93b7e815faa7
samikannup53 <samikannup53@gmail.com>
2025-07-12 19:16:50 +0530

    Debug Cookie Error

--- controllers/authController/authMe.js
+++ controllers/authController/authMe.js
@@ -2,7 +2,4 @@
 function getLoggedInUser(req, res) {
   res.status(200).json({ msg: "User Loggedin", user: req.user });
 }
-
-console.log("Received cookies:", req.cookies);
-
 module.exports = getLoggedInUser;
