e5e32116319bfb715943f37b046027c4c7611b22
samikannup53 <samikannup53@gmail.com>
2025-07-13 19:13:23 +0530

    PDF Error Fix

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,7 @@
+// .puppeteerrc.cjs
+const { join } = require('path');
+
+/** @type {import('puppeteer').Configuration} */
+module.exports = {
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};
